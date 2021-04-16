5478a216cc57d15434ecbc8dae2dba363b6a4668
Sagi <52858682+ratsagi@users.noreply.github.com>
2021-04-16 19:48:22 +0600

    Add files via upload
    My initials written in java code.

---
+++ MyInitials.java
@@ -0,0 +1,14 @@
+public class MyInitials{
+	public static void main(String[] args) {
+     System.out.print("              s s s              RRRRRRR\n");
+     System.out.print("             s      s            R      R\n");
+      System.out.print("             s                  R      R\n");
+       System.out.print("             s                 R R R R R\n");
+        System.out.print("               s              R      R\n");
+        System.out.print("                s             R       R\n");
+         System.out.print("          s s s s            R        R");
+         System.out.print("  ");
+          System.out.print("  ");
+           
+	}
+}
