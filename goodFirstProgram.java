3f6dc875fc52f6e249970be5ba62f366e5545f8d
Sagi <52858682+ratsagi@users.noreply.github.com>
2021-04-16 17:45:34 +0600

    Add files via upload
    Another Hello world java code but this one is a assignment from programminbydoing website.

---
+++ goodFirstProgram.java
@@ -0,0 +1,19 @@
+// This is not my first program in java. 
+//Furthermore this two slashes are used to create 
+//single comments
+public class goodFirstProgram{
+	public static void main(String[] args) {
+	//System.out.println("Hello World!");
+	//System.out.println("My name is Sagi");
+	//System.out.println("I am 23 years old");
+	System.out.print("Hello World! \n");
+	//System.out.print("My favourite activities are \n\t");
+	//System.out.print("1 Coding \n\t");
+	//System.out.print("2 watching an anime  \n\t");
+	//System.out.print("2 Listen to music.");
+
+
+
+
+}
+}
