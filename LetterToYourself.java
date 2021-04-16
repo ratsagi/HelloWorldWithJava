caa4b653b03bfb10e8913b81c6de5defeda7f5d2
Sagi <52858682+ratsagi@users.noreply.github.com>
2021-04-16 18:07:23 +0600

    Add files via upload
    The second assignment from programming by doing!

---
+++ LetterToYourself.java
@@ -0,0 +1,16 @@
+public class LetterToYourself{
+	public static void main(String[] args) {
+		System.out.println("+-----------------------------------+");
+		System.out.println("|                              ###  |");
+		System.out.println("|                              ###  |");
+		System.out.println("|                              ###  |");
+		System.out.println("|                                   |");
+		System.out.println("|                                   |");
+		System.out.println("|             Sagi Rat              |");
+		System.out.println("|             1 java programing way |");
+        System.out.println("|             2903  Sherman Street  |");
+        System.out.println("|                                   |");
+        System.out.println("+-----------------------------------+");
+
+	}
+}
