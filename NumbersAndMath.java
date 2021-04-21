32c559b12e807281504479a8dd135040062960e7
Sagi <52858682+ratsagi@users.noreply.github.com>
2021-04-21 22:08:30 +0600

    Add files via upload
    I could not check the code because I have changed the java 16 to java8 so in my terminal it has not been run but I think it is working code. the exercise from programing by doing.

---
+++ NumbersAndMath.java
@@ -0,0 +1,21 @@
+public class NumbersAndMath{
+	public static void main(String[] args) {
+		System.out.println("I will now count my chickens:");
+
+		System.out.println("Hens"+(25.0+30.0/6.0)); // it divides 30.0 and 6.0 then adds the 25.0
+		System.out.println("Now I will count the eggs");
+		System.out.println(3.0+2.0+1.0-5.0+4.0%2.0-1.0/4.0+6.0);// in this line it counts the eggs
+		System.out.println("Is it true that 3+2<5-7?");
+		System.out.println(3+2<5-7);
+		System.out.println("What is 3+2?"+(3+2));
+		System.out.println("What is 5-7?"+(5-7));
+		System.out.println("Oh that is why it is false.");
+		System.out.println("How about some more.");
+		System.out.println("Is it greater ?"+(5>-2));
+		System.out.println("is it greater or equal?"+(5>=-2));
+		System.out.println("is it less or equal?"+(5<=-2));
+
+
+
+	}
+}
